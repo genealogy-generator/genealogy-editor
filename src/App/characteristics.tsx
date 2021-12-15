de1d4e0d5d1ddfa3f4ee1c33f21cb93721079f8e
jaroslav2324 <jaro_r@mail.ru>
2021-12-15 20:06:21 +0300

    Modified person.tsx

--- src/App/characteristics.tsx
+++ src/App/characteristics.tsx
@@ -1,20 +1,85 @@
-import { tmpdir } from "os";
-import React from "react";
-import Person from "./person";
+import React, { useState } from "react";
 
 function Characteristics() {
+  const [isEditing, setisEditing] = useState(1);
+  const [Text, setText] = useState("Edit");
+
+  function handleClick() {
+    if (isEditing) {
+      setisEditing(0);
+      setText("Edit");
+    } else {
+      setisEditing(1);
+      setText("Save");
+    }
+    //console.log(isEditing);
+  }
+
   return (
-    <div>
-      <label>Name</label>
-      <input id="name"></input>
-      <label>Surname</label>
-      <input id="surname"></input>
-      <label>Patronymic</label>
-      <input id="patronymic"></input>
-      <label>Date of birth</label>
-      <input id="datebirth"></input>
-      <label>Date of death</label>
-      <input id="datedeath"></input>
+    <div style={{ display: "grid" }}>
+      <button onClick={handleClick} style={{ width: "100%", height: "2rem" }}>
+        {Text}
+      </button>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+          Name:{" "}
+        </h4>
+        {isEditing ? (
+          <input style={{ width: "15rem", height: "2rem" }}></input>
+        ) : (
+          <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+            name
+          </h4>
+        )}
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+          Surname:{" "}
+        </h4>
+        {isEditing ? (
+          <input style={{ width: "15rem", height: "2rem" }}></input>
+        ) : (
+          <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+            surname
+          </h4>
+        )}
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+          Patronymic:{" "}
+        </h4>
+        {isEditing ? (
+          <input style={{ width: "15rem", height: "2rem" }}></input>
+        ) : (
+          <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+            patronymic
+          </h4>
+        )}
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+          Date of birth:{" "}
+        </h4>
+        {isEditing ? (
+          <input style={{ width: "15rem", height: "2rem" }}></input>
+        ) : (
+          <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+            datebirth
+          </h4>
+        )}
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+          Date of death:{" "}
+        </h4>
+        {isEditing ? (
+          <input style={{ width: "15rem", height: "2rem" }}></input>
+        ) : (
+          <h4 style={{ width: "12rem", height: "2rem", alignItems: "center" }}>
+            datedeath
+          </h4>
+        )}
+      </div>
     </div>
   );
 }
--- src/App/person.tsx
+++ src/App/person.tsx
@@ -1,19 +1,11 @@
 import TimelineDate from "./timelineDate";
 
-class Person {
+interface Person {
 name : string;
 surname : string;
 patronymic : string;
 birth: TimelineDate | null;
 death: TimelineDate | null;
-
-    constructor() {
-        this.name = "undefined";
-        this.surname = "undefined";
-        this.patronymic = "undefined";
-        this.birth = null;
-        this.death = null;
-    }
 }
 
 export default Person
